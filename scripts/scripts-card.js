15af93569eee49abc20386ef55b2946445617bd8
VERA <viraxyz@ukr.net>
2022-07-06 15:16:03 +0300

    new function

--- scripts/scripts-card.js
+++ scripts/scripts-card.js
@@ -1,17 +1,17 @@
-let tableCards = [
-     [' ','Name','User credentials','Last Transcation','Status','End date ','Total Used',' '],    
-     ['images/vector.png','images/figma.png','Figma','Visa', 'Itai Bracha', 'Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
-     ['https://www.figma.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
-     ['images/vector.png','images/xd.png','Adobe XD','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
-     ['https://www.adobe.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
-     ['images/vector.png','images/mailchimp.png','Mailchimp','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
-     ['https://mailchimp.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
-     ['images/vector.png','images/x.png','WIX','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
-     ['https://www.wix.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
-     ['images/vector.png','images/youtube.png','Youtube','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
-     ['https://www.youtube.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
-     ['images/num0.png','images/num1.png','images/num2.png','images/num3.png','images/num4.png',' ']
-]  
+// let tableCards = [
+//      [' ','Name','User credentials','Last Transcation','Status','End date ','Total Used',' '],    
+//      ['images/vector.png','images/figma.png','Figma','Visa', 'Itai Bracha', 'Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
+//      ['https://www.figma.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
+//      ['images/vector.png','images/xd.png','Adobe XD','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
+//      ['https://www.adobe.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
+//      ['images/vector.png','images/mailchimp.png','Mailchimp','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
+//      ['https://mailchimp.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
+//      ['images/vector.png','images/x.png','WIX','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
+//      ['https://www.wix.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
+//      ['images/vector.png','images/youtube.png','Youtube','Visa','Itai Bracha','Jan 2,2022','Done','Jan 12,2022','$783.22','images/3dot.png'],
+//      ['https://www.youtube.com/','***** 2468','Itai Bracha31@gmail.com','$783.22'],
+//      ['images/num0.png','images/num1.png','images/num2.png','images/num3.png','images/num4.png',' ']
+// ]  
 
 // let tableEcards = [
 //     [[1],[2],[3],[4],[5],[6],[7],[8]],
@@ -68,68 +68,200 @@ let tableCards = [
 
      
     
-    function createThead(myArray) {
-        let theadElement = "<thead>";
-            theadElement += "<tr>";
-            for(let j = 0; j < myArray[0].length; j++){
-                if (j >= 1 && j <= 2) {
-                    theadElement += "<th scope=\"col\" colspan=\"2\">"+myArray[0][j]+"</th>";
-                } else {
-                    theadElement += "<th scope=\"col\">"+myArray[0][j]+"</th>";
-                }    
-            }
-            theadElement += "</tr>";
-            theadElement += "</thead>";
+    // function createThead(myArray) {
+    //     let theadElement = "<thead>";
+    //         theadElement += "<tr>";
+    //         for(let j = 0; j < myArray[0].length; j++){
+    //             if (j >= 1 && j <= 2) {
+    //                 theadElement += "<th scope=\"col\" colspan=\"2\">"+myArray[0][j]+"</th>";
+    //             } else {
+    //                 theadElement += "<th scope=\"col\">"+myArray[0][j]+"</th>";
+    //             }    
+    //         }
+    //         theadElement += "</tr>";
+    //         theadElement += "</thead>";
 
-        return theadElement;
-    }
+    //     return theadElement;
+    // }
 
-    function createTbody(myArray) {
-        let tbodyElement = "<tbody>";
-        for(let i = 1; i < myArray.length - 1; i++){
-            tbodyElement += "<tr>";            
-            for(let j = 0; j < myArray[i].length; j++){
-                let x = myArray[i][j];
-                if (x[x.length - 2] === 'n' && x[x.length - 1] === 'g'){
-                     myArray[i][j] = '<img src=\"' + myArray[i][j] + '\" alt=\"\">';
-                };
+    // function createTbody(myArray) {
+    //     let tbodyElement = "<tbody>";
+    //     for(let i = 1; i < myArray.length - 1; i++){
+    //         tbodyElement += "<tr>";            
+    //         for(let j = 0; j < myArray[i].length; j++){
+    //             let x = myArray[i][j];
+    //             if (x[x.length - 2] === 'n' && x[x.length - 1] === 'g'){
+    //                  myArray[i][j] = '<img src=\"' + myArray[i][j] + '\" alt=\"\">';
+    //             };
 
-                if (j === 0 && i % 2 === 1 ) {
-                    tbodyElement += "<th scope=\"row\" rowspan=\"2\">" + myArray[i][j]+"</th>";
-                } else if (i % 2 === 1 && (j === 1 || j >= 6)) {
-                    tbodyElement += "<td rowspan=\"2\">"+myArray[i][j]+"</td>";
-                } else {
-                    tbodyElement += "<td>"+myArray[i][j]+"</td>";
-                }
-            }        
-            tbodyElement += "</tr>";
-        }
-        tbodyElement += "</tbody>";
+    //             if (j === 0 && i % 2 === 1 ) {
+    //                 tbodyElement += "<th scope=\"row\" rowspan=\"2\">" + myArray[i][j]+"</th>";
+    //             } else if (i % 2 === 1 && (j === 1 || j >= 6)) {
+    //                 tbodyElement += "<td rowspan=\"2\">"+myArray[i][j]+"</td>";
+    //             } else {
+    //                 tbodyElement += "<td>"+myArray[i][j]+"</td>";
+    //             }
+    //         }        
+    //         tbodyElement += "</tr>";
+    //     }
+    //     tbodyElement += "</tbody>";
 
-        return tbodyElement;
-    }
+    //     return tbodyElement;
+    // }
 
-    function createTfoot(myArray) {
-        let tfootElement = "<tfoot>";
-            tfootElement += "<tr>";
-            for(let j = 0; j < myArray[myArray.length - 1].length; j++){
-                tfootElement += "<td>" +'<img src=\"' + myArray[[myArray.length - 1]][j] + '\" alt=\"\">'+ "</td>";
-            }
-            tfootElement += "</tr>";
-            tfootElement += "</tfoot>";
+    // function createTfoot(myArray) {
+    //     let tfootElement = "<tfoot>";
+    //         tfootElement += "<tr>";
+    //         for(let j = 0; j < myArray[myArray.length - 1].length; j++){
+    //             tfootElement += "<td>" +'<img src=\"' + myArray[[myArray.length - 1]][j] + '\" alt=\"\">'+ "</td>";
+    //         }
+    //         tfootElement += "</tr>";
+    //         tfootElement += "</tfoot>";
         
-        return tfootElement;
-    }
+    //     return tfootElement;
+    // }
 
-    function makeTable(myArray) {
-        let result = "<table border=0>";
-        let x = createThead(myArray);
-        let y = createTbody(myArray);
-        let z = createTfoot(myArray);
-        result = result + x + y + z + "</table>";
+    // function makeTable(myArray) {
+    //     let result = "<table border=0>";
+    //     let x = createThead(myArray);
+    //     let y = createTbody(myArray);
+    //     let z = createTfoot(myArray);
+    //     result = result + x + y + z + "</table>";
     
-        return result;
+    //     return result;
+    // };
+    // let myTable = makeTable(tableCards);
+    // let placeForTable = document.getElementById('javaTable');
+    // placeForTable.insertAdjacentHTML('afterbegin', myTable);
+
+
+
+    let tableDynamic = [{
+            chek: 'true',
+            imgCompUrl: 'images/figma.png',
+            companyData: [{
+                companyName: 'Figma',
+                companyUrl: 'https://www.figma.com/'
+            }],
+            cardData: [{
+                cardName: 'Visa',
+                cardNum: '***** 2468'
+            }],
+            userData: [{
+                userName: 'Itai Bracha',
+                userEmail: 'Itai Bracha31@gmail.com'
+            }],
+            lastTrans: 'Jan 2,2022',
+            transStatus: 'Done',
+            endDate: 'Jan 12,2022',
+            totalUsed: '$783.22',
+            imgDotsUrl: 'images/3dot.png'
+        },{
+            chek: 'true',
+            imgCompUrl: 'images/xd.png',
+            companyData: [{
+                companyName: 'Adobe XD',
+                companyUrl: 'https://www.adobe.com/'
+            }],
+            cardData: [{
+                cardName: 'Visa',
+                cardNum: '***** 2468'
+            }],
+            userData: [{
+                userName: 'Itai Bracha',
+                userEmail: 'Itai Bracha31@gmail.com'
+            }],
+            lastTrans: 'Jan 2,2022',
+            transStatus: 'Done',
+            endDate: 'Jan 12,2022',
+            totalUsed: '$783.22',
+            imgDotsUrl: 'images/3dot.png'
+        },{
+            chek: 'true',
+            imgCompUrl: 'images/mailchimp.png',
+            companyData: [{
+                companyName: 'Mailchimp',
+                companyUrl: 'https://mailchimp.com/'
+            }],
+            cardData: [{
+                cardName: 'Visa',
+                cardNum: '***** 2468'
+            }],
+            userData: [{
+                userName: 'Itai Bracha',
+                userEmail: 'Itai Bracha31@gmail.com'
+            }],
+            lastTrans: 'Jan 2,2022',
+            transStatus: 'Done',
+            endDate: 'Jan 12,2022',
+            totalUsed: '$783.22',
+            imgDotsUrl: 'images/3dot.png'
+        },{
+            chek: 'true',
+            imgCompUrl: 'images/x.png',
+            companyData: [{
+                companyName: 'WIX',
+                companyUrl: 'https://www.wix.com/'
+            }],
+            cardData: [{
+                cardName: 'Visa',
+                cardNum: '***** 2468'
+            }],
+            userData: [{
+                userName: 'Itai Bracha',
+                userEmail: 'Itai Bracha31@gmail.com'
+            }],
+            lastTrans: 'Jan 2,2022',
+            transStatus: 'Done',
+            endDate: 'Jan 12,2022',
+            totalUsed: '$783.22',
+            imgDotsUrl: 'images/3dot.png'
+        },{
+            chek: 'true',
+            imgCompUrl: 'images/youtube.png',
+            companyData: [{
+                companyName: 'Youtube',
+                companyUrl: 'https://www.youtube.com/'
+            }],
+            cardData: [{
+                cardName: 'Visa',
+                cardNum: '***** 2468'
+            }],
+            userData: [{
+                userName: 'Itai Bracha',
+                userEmail: 'Itai Bracha31@gmail.com'
+            }],
+            lastTrans: 'Jan 2,2022',
+            transStatus: 'Done',
+            endDate: 'Jan 12,2022',
+            totalUsed: '$783.22',
+            imgDotsUrl: 'images/3dot.png'
+        }]
+
+
+    function createDynamicTable(tableData){
+        let myTable = document.createElement('table');
+        let tableBody = document.createElement('tbody');        
+
+        for( let i=0; i < tableData.length; i++){
+            let tableRows = document.createElement('tr');
+            for(let j=0; j < tableData[i].length; j++){
+                 if (Array.isArray(tableData[j]) === true){
+                    for(let x=0; x<tableData[i][j].length; x++){
+                    let cellData = document.createTextNode('"<td>"+tableData[i][j][x].key+"</td>"');
+                    }
+                } else {
+                        cellData = document.createTextNode('"<td rowspan = \"2\">"+tableData[i][j].key+"</td>"');
+                    }
+                    tableRows.appendChild(cellData);                     
+            }                
+        }
+        tableBody.appendChild(tableRows);
+    
+        myTable.appendChild(tableBody);
+        let placeForTable = document.getElementById(tableObjs);
+        placeForTable.appendChild(myTable);
+            
     };
-    let myTable = makeTable(tableCards);
-    let placeForTable = document.getElementById('javaTable');
-    placeForTable.insertAdjacentHTML('afterbegin', myTable);
+
+  createDynamicTable(tableDynamic);  
